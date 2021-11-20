75d403f8324a6ded5017bb34613cb34c7441d716
ampersand-github <ampersand.dev@gmail.com>
2021-11-20 19:04:52 +0900

    契約内容

---
+++ backend/src/domain/contact-details/__interface__/contact-details-domain-interface.ts
@@ -0,0 +1,3 @@
+export interface IContactDetailsDomain {
+  name: string;
+}
---
+++ backend/src/domain/contact-details/__tests__/contact-details-id.unit.test.ts
@@ -0,0 +1,24 @@
+import { ContactDetailsId } from "../contact-details-id";
+
+describe('ContactDetailsId', () => {
+  it('createできる', () => {
+    expect(ContactDetailsId.create()).toEqual(expect.any(ContactDetailsId));
+  });
+
+  it('restoreできる', () => {
+    expect(ContactDetailsId.restore('test-id')).toEqual(expect.any(ContactDetailsId));
+  });
+
+  it('toStringできる', () => {
+    expect(ContactDetailsId.restore('test-id').toString()).toEqual('test-id');
+  });
+
+  it('equalsできる', () => {
+    expect(
+      ContactDetailsId.restore('test-id').equals(ContactDetailsId.restore('test-id')),
+    ).toBe(true);
+    expect(
+      ContactDetailsId.restore('test-id').equals(ContactDetailsId.restore('not-equal-id')),
+    ).toBe(false);
+  });
+});
