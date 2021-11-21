b55ee0855cab0a07797b3e86e437346eb6a4b594
ampersand-github <ampersand.dev@gmail.com>
2021-11-21 22:07:42 +0900

    機器

---
+++ backend/src/usecase/instrument/find-by-id-usecase.ts
@@ -0,0 +1,13 @@
+import { Instrument } from 'src/domain/Instrument/Instrument'
+import { InstrumentId } from 'src/domain/Instrument/Instrument-id'
+import { IInstrumentRepository } from 'src/domain/Instrument/__interface__/Instrument-repository-interface'
+import { InstrumentOutputDto } from './__dto__/instrument-output-dto'
+
+export class FindByIdUsecase {
+  public constructor (readonly repository: IInstrumentRepository) {}
+
+  public async do (id: string): Promise<InstrumentOutputDto> {
+    const instrument: Instrument = await this.repository.findById(InstrumentId.restore(id))
+    return new InstrumentOutputDto(instrument)
+  }
+}
