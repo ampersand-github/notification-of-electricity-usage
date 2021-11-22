0ecc6d28110bbfe956d0060d072d8931c82760ab
ampersand-github <ampersand.dev@gmail.com>
2021-11-22 23:49:30 +0900

    output修正

--- backend/src/usecase/cusotmer/__dto__/customer-output-dto.ts
+++ backend/src/usecase/cusotmer/__dto__/customer-output-dto.ts
@@ -1,4 +1,4 @@
-import { Customer } from 'src/domain/customer/customer'
+import { Customer } from "src/domain/customer/customer";
 
 export class CustomerOutputDto {
   public readonly name: string;
@@ -6,11 +6,20 @@ export class CustomerOutputDto {
   public readonly meterDate: number;
   public readonly meterPlace: number;
   public readonly othersNumber: number;
-  public constructor (customer: Customer) {
-    this.name = customer.name
-    this.address = customer.address
-    this.meterDate = customer.customerNumber.meterDate
-    this.meterPlace = customer.customerNumber.meterPlace
-    this.othersNumber = customer.customerNumber.othersNumber
+  public readonly monthHistoryId: string;
+  public readonly supplyPointSpecificNumberId: string;
+  public readonly contractDetailsId: string;
+  public readonly instrumentId: string;
+  public constructor(customer: Customer) {
+    this.name = customer.name;
+    this.address = customer.address;
+    this.meterDate = customer.customerNumber.meterDate;
+    this.meterPlace = customer.customerNumber.meterPlace;
+    this.othersNumber = customer.customerNumber.othersNumber;
+    this.monthHistoryId = customer.monthHistoryId.toString();
+    this.supplyPointSpecificNumberId =
+      customer.supplyPointSpecificNumberId.toString();
+    this.contractDetailsId = customer.contractDetailsId.toString();
+    this.instrumentId = customer.instrumentId.toString();
   }
 }
