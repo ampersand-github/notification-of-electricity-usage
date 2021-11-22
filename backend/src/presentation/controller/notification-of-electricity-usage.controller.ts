ab48c55708a4b8f87f89270edfb0c2ed3195b72f
ampersand-github <ampersand.dev@gmail.com>
2021-11-22 23:48:51 +0900

    途中

--- backend/src/presentation/controller/notification-of-electricity-usage.controller.ts
+++ backend/src/presentation/controller/notification-of-electricity-usage.controller.ts
@@ -1,46 +1,51 @@
-import {Controller, Get, HttpException, HttpStatus, UseGuards} from '@nestjs/common'
-import { Customer } from 'src/domain/customer/customer'
-import { CustomerId } from 'src/domain/customer/customer-id'
-import {AuthGuard} from "../decorator/guards/auth.guard";
-import {PrismaService} from "../../infrastructure/prisma/prisma.service";
-import {CustomerRepository} from "../../infrastructure/mock-repository/customer-repository";
-import {MonthHistoryRepository} from "../../infrastructure/mock-repository/month-history-repository";
-import {CustomerFindByIdUsecase} from "../../usecase/cusotmer/customer-find-by-id-usecase";
-import {MonthHistoryFindByIdUsecase} from "../../usecase/month-history/month-history-find-by-id-usecase";
-import {CustomerOutputDto} from "../../usecase/cusotmer/__dto__/customer-output-dto";
-import {MonthHistoryOutputDto} from "../../usecase/month-history/__dto__/month-history-output-dto";
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  UseGuards,
+} from "@nestjs/common";
+import { PrismaService } from "src/infrastructure/prisma/prisma.service";
+import { AuthGuard } from "../decorator/guards/auth.guard";
+import { CustomerFindByIdUsecase } from "src/usecase/cusotmer/customer-find-by-id-usecase";
+import { MockMonthHistoryRepository } from "src/infrastructure/mock-repository/mock-month-history-repository";
+import { MockInstrumentRepository } from "src/infrastructure/mock-repository/mock-Instrument-repository";
+import { FindLatestUsecase } from "src/usecase/month-history/find-latest-usecase";
+import { InstrumentFindByIdUsecase } from "src/usecase/instrument/instrument-find-by-id-usecase";
+import { MockCustomerRepository } from "src/infrastructure/mock-repository/mock-customer-repository";
 
 @UseGuards(AuthGuard)
 @Controller("notification-of-electricity-usage")
 export class NotificationOfElectricityUsageController {
   prismaService = new PrismaService();
-  customerRepository = new CustomerRepository(this.prismaService);
-  monthHistoryRepository = new MonthHistoryRepository(this.prismaService);
-
-  @Get('latest')
-  async getLatest(): Promise<{ customer: string, thisHistory: string, lastHistory: string }> {
+  customerRepository = new MockCustomerRepository(this.prismaService);
+  monthHistoryRepository = new MockMonthHistoryRepository(this.prismaService);
+  instrumentRepository = new MockInstrumentRepository(this.prismaService);
+  @Get("latest")
+  async getLatest(): Promise<{
+    customer: string;
+    thisHistory: string;
+    lastHistory: string;
+  }> {
     try {
-      const id = "" // uid
-      const customerFindByIdUsecase = new CustomerFindByIdUsecase(this.customerRepository)
-      const monthHistoryFindByIdUsecase = new MonthHistoryFindByIdUsecase(this.monthHistoryRepository)
-      const customerOutputDto: CustomerOutputDto = await customerFindByIdUsecase.do(id)
-      const monthHistoryOutputDto: MonthHistoryOutputDto = await monthHistoryFindByIdUsecase.do(id)
-
-      const thisMonthHistoryDTO = '' // idをhistoryのユースケースに渡して返却された値
-      const lastMonthHistoryDTO = '' // idをhistoryのユースケースに渡して返却された値
+      const id = ""; // uid
+      const customerFindByIdUsecase = new CustomerFindByIdUsecase(this.customerRepository);
+      const findLatestUsecase = new FindLatestUsecase(this.monthHistoryRepository);
+      const instrumentFindByIdUsecase = new InstrumentFindByIdUsecase(this.instrumentRepository);
+      const customerOutputDto = await customerFindByIdUsecase.do(id);
+      const monthHistoryOutputDto = await findLatestUsecase.do(id);
+      const instrumentOutputDto = await instrumentFindByIdUsecase.do(customerOutputDto.instrumentId);
       // CQRSで特定地点番号　customerのidと供給地点特定番号から取得　これはなんだ？CQRS？ドメインサービス？ -> customerのドメインサービスかも
       // const 検針員名
-      // const 計器番号
 
       // これらの汎用DTOをまとめて電気料金使用のお知らせ専用DTOをつくる
-
     } catch (error) {
-      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
     return {
-      customer: '',
-      thisHistory: '',
-      lastHistory: ''
-    }
+      customer: "",
+      thisHistory: "",
+      lastHistory: "",
+    };
   }
 }
