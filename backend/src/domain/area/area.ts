d1c1fab6af13550205d62408aae7c7cd7b4b6a03
ampersand-github <ampersand.dev@gmail.com>
2021-11-22 23:55:05 +0900

    フォーマッター

--- backend/src/domain/area/area.ts
+++ backend/src/domain/area/area.ts
@@ -1,22 +1,22 @@
-import { AggregateRoot } from "../__shared__/aggregate-root";
-import { AreaId } from "./area-id";
-import { IAreaDomain } from "./__interface__/area-domain-interface";
-import { ICodeDomain } from "./__interface__/code-domain-interface";
+import { AggregateRoot } from '../__shared__/aggregate-root'
+import { AreaId } from './area-id'
+import { IAreaDomain } from './__interface__/area-domain-interface'
+import { ICodeDomain } from './__interface__/code-domain-interface'
 
 export class Area extends AggregateRoot<IAreaDomain, AreaId> {
-  public static create(props: IAreaDomain): Area {
-    return new Area(props, AreaId.create());
+  public static create (props: IAreaDomain): Area {
+    return new Area(props, AreaId.create())
   }
 
-  public static restore(props: IAreaDomain, id: AreaId): Area {
-    return new Area(props, id);
+  public static restore (props: IAreaDomain, id: AreaId): Area {
+    return new Area(props, id)
   }
 
-  public get code(): ICodeDomain["code"] {
-    return this.props.code.code;
+  public get code (): ICodeDomain['code'] {
+    return this.props.code.code
   }
 
-  public get name(): IAreaDomain["name"] {
-    return this.props.name;
+  public get name (): IAreaDomain['name'] {
+    return this.props.name
   }
 }
