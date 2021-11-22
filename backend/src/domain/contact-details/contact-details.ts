c7320c482d1dd6433593528c5f2c41a6c32ec05a
ampersand-github <ampersand.dev@gmail.com>
2021-11-22 23:55:22 +0900

    フォーマッター

--- backend/src/domain/contact-details/contact-details.ts
+++ backend/src/domain/contact-details/contact-details.ts
@@ -2,12 +2,18 @@ import { AggregateRoot } from '../__shared__/aggregate-root'
 import { ContactDetailsId } from './contact-details-id'
 import { IContactDetailsDomain } from './__interface__/contact-details-domain-interface'
 
-export class ContactDetails extends AggregateRoot<IContactDetailsDomain, ContactDetailsId> {
+export class ContactDetails extends AggregateRoot<
+  IContactDetailsDomain,
+  ContactDetailsId
+> {
   public static create (props: IContactDetailsDomain): ContactDetails {
     return new ContactDetails(props, ContactDetailsId.create())
   }
 
-  public static restore (props: IContactDetailsDomain, id: ContactDetailsId): ContactDetails {
+  public static restore (
+    props: IContactDetailsDomain,
+    id: ContactDetailsId
+  ): ContactDetails {
     return new ContactDetails(props, id)
   }
 
