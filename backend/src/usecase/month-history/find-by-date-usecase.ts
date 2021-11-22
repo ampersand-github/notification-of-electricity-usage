2e8c1320d842da7630744c2145667c64d1b7f681
ampersand-github <ampersand.dev@gmail.com>
2021-11-22 23:49:38 +0900

    ユースケース作成

---
+++ backend/src/usecase/month-history/find-by-date-usecase.ts
@@ -0,0 +1,21 @@
+import { Month } from "src/domain/month-history/month";
+import { MonthHistoryId } from "src/domain/month-history/month-history-id";
+import { Year } from "src/domain/month-history/year";
+import { IMonthHistoryRepository } from "src/domain/month-history/__interface__/month-history-repository-interface";
+import { MonthHistoryOutputDto } from "./__dto__/month-history-output-dto";
+
+export class FindByDateUsecase {
+  public constructor(readonly repository: IMonthHistoryRepository) {}
+
+  public async do(
+    id: string,
+    year: number,
+    month: number
+  ): Promise<MonthHistoryOutputDto> {
+    const monthHistoryId = MonthHistoryId.restore(id);
+    const _year: Year = Year.create({ year: year });
+    const _month: Month = Month.create({ month: month });
+    const history = await this.repository.findByDate(monthHistoryId, _year, _month);
+    return new MonthHistoryOutputDto(history);
+  }
+}
--- backend/src/usecase/month-history/find-latest-usecase.ts
+++ backend/src/usecase/month-history/find-latest-usecase.ts
@@ -3,11 +3,11 @@ import { MonthHistoryId } from 'src/domain/month-history/month-history-id'
 import { IMonthHistoryRepository } from 'src/domain/month-history/__interface__/month-history-repository-interface'
 import { MonthHistoryOutputDto } from './__dto__/month-history-output-dto'
 
-export class MonthHistoryFindByIdUsecase {
+export class FindLatestUsecase {
   public constructor (readonly repository: IMonthHistoryRepository) {}
 
   public async do (id: string): Promise<MonthHistoryOutputDto> {
-    const history: MonthHistory = await this.repository.findById(MonthHistoryId.restore(id))
+    const history: MonthHistory = await this.repository.findLatest(MonthHistoryId.restore(id))
     return new MonthHistoryOutputDto(history)
   }
 }
