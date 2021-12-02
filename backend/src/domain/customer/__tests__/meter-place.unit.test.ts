275dc4c143bcb71d18c3fad7f1033f03dbdf6d0c
ampersand-github <ampersand.dev@gmail.com>
2021-12-03 02:22:02 +0900

    間違ってmainブランチにコミットしていたので元に戻す

--- backend/src/domain/customer/__tests__/meter-place.unit.test.ts
+++ backend/src/domain/customer/__tests__/meter-place.unit.test.ts
@@ -1,14 +1,15 @@
-import { MeterPlace, MeterPlaceProps } from '../meter-place'
+import { MeterPlace } from '../meter-place'
+import {IMeterPlaceDomain} from "../__interface__/meter-place-domain-interface";
 
 describe('MeterPlace', () => {
   it('3桁はエラー', () => {
-    const data: MeterPlaceProps = { meterPlace: 100 }
+    const data: IMeterPlaceDomain = { meterPlace: 100 }
     expect(() => {
       MeterPlace.create(data)
     }).toThrowError()
   })
   it('2桁は正常', () => {
-    const data: MeterPlaceProps = { meterPlace: 99 }
+    const data: IMeterPlaceDomain = { meterPlace: 99 }
     expect(MeterPlace.create(data)).toEqual(expect.any(MeterPlace))
   })
 })
--- backend/src/domain/month-history/__interface__/reading-date-domain-interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IReadingDateDomain {
-  readingDate: Date;
-}
